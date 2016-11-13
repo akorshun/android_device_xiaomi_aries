dd14c76b669537d6e8dd08381efe75087c07b7a6
akorshun <dron1702-1997@bk.ru>
2016-11-13 17:34:28 +0500

    init for aries

---
+++ BoardConfig.mk
@@ -0,0 +1,230 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/xiaomi/aries
+
+TARGET_BOOTLOADER_NAME       := aries
+TARGET_BOARD_INFO_FILE       := $(LOCAL_PATH)/board-info.txt
+
+TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
+
+# Audio
+TARGET_QCOM_AUDIO_VARIANT := caf
+BOARD_HAVE_AUDIENCE_ES310 := true
+BOARD_HAVE_NEW_QCOM_CSDCLIENT   := true
+BOARD_HAVE_CSD_FAST_CALL_SWITCH := true
+
+BOARD_USES_ALSA_AUDIO               := true
+TARGET_USES_QCOM_MM_AUDIO           := true
+TARGET_USES_QCOM_COMPRESSED_AUDIO   := true
+BOARD_USES_LEGACY_ALSA_AUDIO        := true
+QCOM_ANC_HEADSET_ENABLED            := true
+QCOM_FLUENCE_ENABLED                := false
+TUNNEL_MODE_SUPPORTS_AMRWB          := true
+USE_TUNNEL_MODE                     := true
+QCOM_TUNNEL_LPA_ENABLED             := true
+QCOM_PROXY_DEVICE_ENABLED           := true
+QCOM_ACDB_ENABLED                   := true
+QCOM_AUDIO_FORMAT_ENABLED           := true
+QCOM_CSDCLIENT_ENABLED              := true
+AUDIO_FEATURE_ENABLED_PROXY_DEVICE  := true
+AUDIO_FEATURE_ENABLED_AUXPCM_BT     := false
+AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
+AUDIO_FEATURE_ENABLED_EXTN_FORMATS  := true
+QCOM_OUTPUT_FLAGS_ENABLED           := true
+QCOM_USBAUDIO_ENABLED               := true
+QCOM_FLUENCE_ENABLED                := true
+QCOM_MULTI_VOICE_SESSION_ENABLED    := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+BOARD_HAVE_BLUETOOTH       := true
+BOARD_HAVE_BLUETOOTH_QCOM  := true
+BLUETOOTH_HCI_USE_MCT      := true
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# CM Hardware
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 31457280          # 32MB
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640      # 16MB
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824      # 1073MB
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 3758096384    # 3.5GB
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608        # 8MB
+BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184        # 384MB
+BOARD_FLASH_BLOCK_SIZE := 131072                    # (BOARD_KERNEL_PAGESIZE * 64)
+
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_BOOTLOADER := true
+
+QCOM_BOARD_PLATFORMS         := msm8960
+TARGET_BOARD_PLATFORM        := msm8960
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+
+# Flags
+TARGET_GLOBAL_CFLAGS   += -DQCOM_HARDWARE
+TARGET_GLOBAL_CPPFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS   += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+
+# Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_aries.c
+
+# Architecture
+TARGET_CPU_ABI             := armeabi-v7a
+TARGET_CPU_ABI2            := armeabi
+TARGET_CPU_SMP             := true
+TARGET_CPU_VARIANT         := krait
+TARGET_ARCH                := arm
+TARGET_ARCH_VARIANT        := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+BOARD_USES_QCOM_HARDWARE   := true
+
+# Krait optimizations
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET            := true
+TARGET_KRAIT_BIONIC_PLDOFFS         := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH       := 10
+TARGET_KRAIT_BIONIC_BBTHRESH        := 64
+TARGET_KRAIT_BIONIC_PLDSIZE         := 64
+
+# boot image
+BOARD_KERNEL_BASE     := 0x80200000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET  := 0x02000000
+BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000
+BOARD_KERNEL_CMDLINE  := console=null androidboot.hardware=qcom ehci-hcd.park=3 maxcpus=2 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+TARGET_PREBUILT_KERNEL :=
+
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_WLAN_DEVICE                := qcwcn
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+TARGET_USES_WCNSS_CTRL           := true
+TARGET_USES_QCOM_WCNSS_QMI       := true
+TARGET_WCNSS_MAC_PREFIX          := e8bba8
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+
+# FM
+COMMON_GLOBAL_CFLAGS     += -DQCOM_FM_ENABLED
+QCOM_FM_ENABLED          := true
+BOARD_HAVE_QCOM_FM       := true
+AUDIO_FEATURE_ENABLED_FM := true
+
+# QCOM BSP
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+TARGET_USES_QCOM_BSP := true
+
+# QCOM enhanced A/V
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# Display
+TARGET_QCOM_MEDIA_VARIANT       := caf
+TARGET_QCOM_DISPLAY_VARIANT     := caf
+TARGET_USES_ION                 := true
+USE_OPENGL_RENDERER             := true
+TARGET_USES_C2D_COMPOSITION     := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# GPU
+OVERRIDE_RS_DRIVER              := libRSDriver_adreno.so
+HAVE_ADRENO_SOURCE              := false
+BOARD_EGL_CFG                   := $(LOCAL_PATH)/configs/egl.cfg
+
+# GPS
+#The below will be needed if we ever want to build GPS HAL from source
+#TARGET_PROVIDES_GPS_LOC_API := true
+#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+#TARGET_NO_RPC := true
+
+# Webkit
+ENABLE_WEBGL            := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB            := $(LOCAL_PATH)/rootdir/root/fstab.qcom
+
+# Lights HAL
+TARGET_PROVIDES_LIBLIGHT := true
+
+# SELinux
+-include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/aries/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+       atfwd.te \
+       btnvtool.te \
+       camera.te \
+       file.te \
+       file_contexts \
+       init.te \
+       mdm_helper.te \
+       mediaserver.te \
+       sensors.te \
+       system_server.te \
+       thermal-engine.te \
+       ueventd.te \
+       untrusted_app.te
+
+BOARD_USES_SECURE_SERVICES := true
+
+SUPERUSER_EMBEDDED := true
+
+PRODUCT_BOOT_JARS += \
+    qcom.fmradio \
+    qcmediaplayer \
+    org.codeaurora.Performance \
+    tcmiface
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# legacy support flags
+TARGET_USES_LOGD := false
+
+MALLOC_IMPL := dlmalloc
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
+# Try to build the kernel
+TARGET_KERNEL_SOURCE := kernel/xiaomi/aries
+TARGET_KERNEL_CONFIG := aries-perf_defconfig
+
+# Use CM PowerHAL by default
+TARGET_POWERHAL_VARIANT := cm
+
+-include vendor/xiaomi/aries/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,295 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file includes all definitions that apply to ALL aries devices, and
+# are also specific to aries devices
+#
+# Everything in this directory will become public
+
+# Common QCOM configuration tools
+$(call inherit-product, device/qcom/common/Android.mk)
+
+LOCAL_PATH := device/xiaomi/aries
+
+# include additional build utilities
+-include device/xiaomi/aries/utils.mk
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# This device is xhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Wi-Fi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+PRODUCT_PACKAGES += \
+    libwcnss_qmi \
+    wcnss_service
+
+# Hostapd
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_cm.xml:system/etc/media_codecs_xiaomi_extra.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# GPS configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
+
+# mpdecision configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermald-8064.conf:system/etc/thermald-8064.conf \
+    $(LOCAL_PATH)/configs/thermal/thermald-8064ab.conf:system/etc/thermald-8064ab.conf \
+    $(LOCAL_PATH)/configs/thermal/thermald-8960.conf:system/etc/thermald-8960.conf \
+    $(LOCAL_PATH)/configs/thermal/thermald-8960ab.conf:system/etc/thermald-8960ab.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-8064.conf:system/etc/thermal-engine-8064.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-8064ab.conf:system/etc/thermal-engine-8064ab.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-8960.conf:system/etc/thermal-engine-8960.conf
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.print.xml:system/etc/permissions/android.software.print.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# System Properties
+-include $(LOCAL_PATH)/system_prop.mk
+
+# USB, MTP
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp \
+    ro.vold.umsdirtyratio=50 \
+    persist.sys.isUsbOtgEnabled=1
+
+PRODUCT_PACKAGES += \
+    Stk \
+    MiSettings \
+    FileExplorer \
+    Updater
+
+# App
+PRODUCT_PACKAGES += \
+    busybox \
+    CellBroadcastReceiver
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    e2fsck \
+    setup_fs \
+    mke2fs \
+    tune2fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    ntfsfix \
+    ntfs-3g \
+    mkntfs \
+    dumpe2fs \
+    resize2fs \
+    e2fsck_static \
+    mke2fs_static \
+    resize2fs_static \
+    mkfs.f2fs
+
+# QCOM Display
+PRODUCT_PACKAGES += \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils \
+    libtilerenderer \
+    libI420colorconvert \
+    hwcomposer.msm8960 \
+    gralloc.msm8960 \
+    copybit.msm8960 \
+    memtrack.msm8960 \
+    libemoji \
+    lights.msm8960
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio_policy.msm8960 \
+    audio.primary.msm8960 \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    tinymix
+
+# Omx
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libc2dcolorconvert \
+    libdashplayer \
+    libdivxdrmdecrypt \
+    libmm-omxcore \
+    libstagefrighthw
+
+# fmradio support
+PRODUCT_PACKAGES += \
+    qcom.fmradio \
+    libqcomfm_jni \
+    FM2 \
+    FMRecord
+
+# CodeAurora
+PRODUCT_PACKAGES += \
+    qcmediaplayer \
+    org.codeaurora.Performance \
+    BluetoothExt \
+    javax.btobex \
+    libattrib_static \
+    libQWiFiSoftApCfg \
+    libqsap_sdk \
+    QualcommSoftAP \
+    libgestures \
+    gestures.msm8960 \
+    libxml2 \
+    libcnefeatureconfig \
+    services-ext \
+    services \
+    ethernet-service \
+    wifi-service \
+    tcmiface
+
+# Qualcomm random numbers generated
+PRODUCT_PACKAGES += \
+    qrngd
+
+# Power
+PRODUCT_PACKAGES += \
+    power.msm8960
+
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf \
+    dhcpcd.conf \
+    wcnss-service \
+    libwifi-hal \
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.class_main.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.rc \
+    init.qcom.sh \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    init.target.rc \
+    ueventd.qcom.rc
+
+# Init scripts in /etc
+PRODUCT_PACKAGES += \
+    init.qcom.post_boot.sh \
+    init.qcom.fm.sh \
+    init.qcom.bt.sh \
+    init.qcom.btdun.sh \
+    init.qcom.mdm_links.sh \
+    init.qcom.modem_links.sh \
+    init.qcom.syspart_fixup.sh
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8960
+
+# for expanding the system partition
+PRODUCT_PACKAGES += \
+    resize2fs_static
+
+# Prebuilt kl and kcm keymaps
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/apq8064-tabla-snd-card_Button_Jack.kl:system/usr/keylayout/apq8064-tabla-snd-card_Button_Jack.kl \
+    $(LOCAL_PATH)/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/atmel_mxt_ts.kl \
+    $(LOCAL_PATH)/keylayout/cyttsp-i2c.kl:system/usr/keylayout/cyttsp-i2c.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl \
+    $(LOCAL_PATH)/keylayout/keypad_8960_liquid.kl:system/usr/keylayout/keypad_8960_liquid.kl \
+    $(LOCAL_PATH)/keylayout/philips_remote_ir.kl:system/usr/keylayout/philips_remote_ir.kl \
+    $(LOCAL_PATH)/keylayout/samsung_remote_ir.kl:system/usr/keylayout/samsung_remote_ir.kl \
+    $(LOCAL_PATH)/keylayout/sensor00fn1a.kl:system/usr/keylayout/sensor00fn1a.kl \
+    $(LOCAL_PATH)/keylayout/ue_rf4ce_remote.kl:system/usr/keylayout/ue_rf4ce_remote.kl \
+    $(LOCAL_PATH)/keylayout/ft5x06.kl:system/usr/keylayout/ft5x06.kl
+
+# This is the aries-specific audio package
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
+
+$(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
